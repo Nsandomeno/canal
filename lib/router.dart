cb3d23d613fa7b9a5caae7861006f7666e762264
Nicholas Sandomeno <nsando61@gmail.com>
2023-12-21 17:12:41 -0500

    firebase configure and go router

---
+++ lib/router.dart
@@ -0,0 +1,32 @@
+import 'screens/landing.dart';
+import 'screens/profile.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum Routes {
+  landing,
+  profile,
+}
+
+
+final routerProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: "landing",
+    debugLogDiagnostics: true,
+    redirect: (context, state) {
+      return null;
+    },
+    routes: [
+      GoRoute(
+        path: "/landing",
+        name: Routes.landing.name,
+        builder: (context, state) => const Landing()
+      ),
+      GoRoute(
+        path: "/profile",
+        name: Routes.profile.name,
+        builder: (context, state) => const Profile(),
+      )
+    ],
+  );
+});
---
+++ lib/screens/profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class Profile extends ConsumerWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome!"),
+      ),
+      body: const Center(
+        child: Text("Coming Soon..."),
+      ),
+    );
+  }
+}
