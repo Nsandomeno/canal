8db154f3ab82a89c7931d954661e81a753b97866
Nicholas Sandomeno <nsando61@gmail.com>
2024-01-04 22:12:31 -0500

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-import 'router/router.dart';
 import 'package:flutter/material.dart';
-import 'package:canal/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-Future<void> setupEmulators() async {
-  await FirebaseAuth.instance.useAuthEmulator("127.0.0.1", 9099);
-  FirebaseFirestore.instance.useFirestoreEmulator("127.0.0.1",8080);
-}
+import 'package:canal/firebase_options.dart';
+import 'package:canal/initialization/bootstrap/bootstrap.dart';
+import 'package:canal/initialization/bootstrap/firebase_bootstrap.dart';
+// ignore:depend_on_referenced_packages
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
-  // init firebase
   WidgetsFlutterBinding.ensureInitialized();
+  /// init firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await setupEmulators();
-  // run application
-  runApp(const ProviderScope(child: App()));
-}
-
-class App extends ConsumerWidget {
-  const App({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(routerProvider);
-
-    return MaterialApp.router(
-        title: 'Canal',
-        routerConfig: router,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
-          useMaterial3: true,
-        )
-    );
-  }
+  /// * uncomment this if you need to sign out when switching between Firebase projects (emulator <--> firebase backend)
+  /// await FirebaseAuth.instance.signOut();
+  
+  /// turn off the # in the URLs on the web
+  usePathUrlStrategy();
+  /// ensre URL changes  in the address bar when using push / pushNamed
+  /// more info here:
+  /// * https://docs.google.com/document/d/1VCuB85D5kYxPR3qYOjVmw8boAGKb7k62heFyfFHTOvw/edit
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  /// create an app bootstrap instance
+  final appBootstrap = AppBootstrap();
+  /// * uncomment this to connect to the Firebase emulators
+  appBootstrap.setupEmulators();
+  /// create a container configured with all the Firebase repositories
+  final container = await appBootstrap.createFirebaseProviderContainer();
+  /// use the container above to create the root widget
+  final root = appBootstrap.createRootWidget(container: container);
+  /// start the app
+  runApp(root);
 }
